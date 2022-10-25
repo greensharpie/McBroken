2fb48a5a70c579c2106e0fdfffb1bfcc1428010c
Kevin Blanco <kevinjblanco@gmail.com>
2022-10-24 22:31:25 -0400

    created routes to show neighborhoods

---
+++ server.js
@@ -0,0 +1,25 @@
+const { Neighborhood } = require('./models')
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const PORT = process.env.PORT || 3001
+const db = require('./db')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.send('This is root!')
+})
+
+app.get('/neighborhoods', async (req, res) => {
+  const neighborhoods = await Neighborhood.find({})
+  res.json(neighborhoods)
+})
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`)
+})
